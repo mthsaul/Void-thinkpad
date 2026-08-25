5c3d7b398287768da65ac5e5114074d0f96cfcb5
mthsaul <saulllerena29@gmail.com>
2026-08-24 21:27:11 -0500

    Quitar notificacion al cambiar tema, boton waybar funcional con setsid

--- theme-switch.sh
+++ theme-switch.sh
@@ -8,4 +8,3 @@ swww img "$IMG" --transition-type wipe --transition-duration 1
 pkill waybar
 nohup waybar -c "$HOME/.config/waybar/config-green" -s "$HOME/.config/waybar/style-green.css" >> /tmp/waybar-autostart.log 2>&1 </dev/null &
 
-notify-send "Tema aplicado" "$(basename "$IMG")" 2>/dev/null
