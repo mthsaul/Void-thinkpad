647b682e11673d341a942c19335c823e14086be6
mthsaul <saulllerena29@gmail.com>
2026-08-24 20:36:40 -0500

    Usar swww con transición de fade, agregar precache de wallpapers

---
+++ theme-switch.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+IMG="$1"
+[ -z "$IMG" ] && exit 1
+
+wal -i "$IMG" -n -q --saturate 0.3
+swww img "$IMG" --transition-type fade --transition-duration 1
+
+pkill waybar
+nohup waybar -c "$HOME/.config/waybar/config-green" -s "$HOME/.config/waybar/style-green.css" >> /tmp/waybar-autostart.log 2>&1 </dev/null &
+
+notify-send "Tema aplicado" "$(basename "$IMG")" 2>/dev/null
