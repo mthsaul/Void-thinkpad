37463f0cd2d598b8c5eac5f1e93c85c6c5ff62a6
mthsaul <saulllerena29@gmail.com>
2026-08-24 20:48:20 -0500

    Corregir transición a wipe (fade no funcionaba en este hardware)

--- theme-switch.sh
+++ theme-switch.sh
@@ -3,7 +3,7 @@ IMG="$1"
 [ -z "$IMG" ] && exit 1
 
 wal -i "$IMG" -n -q --saturate 0.3
-swww img "$IMG" --transition-type fade --transition-duration 1
+swww img "$IMG" --transition-type wipe --transition-duration 1
 
 pkill waybar
 nohup waybar -c "$HOME/.config/waybar/config-green" -s "$HOME/.config/waybar/style-green.css" >> /tmp/waybar-autostart.log 2>&1 </dev/null &
